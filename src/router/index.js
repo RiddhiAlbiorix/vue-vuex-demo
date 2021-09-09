d4bfc36899f499e1a00d3bfdfb2c0b808b9c372f
riddhi <riddhi.albiorix@gmail.com>
2021-09-09 18:29:36 +0530

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue';
+import NProgress from 'nprogress';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: () => import('../views/products/ProductList.vue')
+  },
+  {
+    path: '/products/create',
+    name: 'Product Create',
+    component: () => import('../views/products/ProductCreate.vue')
+  },
+  {
+    path: '/products/edit/:id',
+    name: 'ProductEdit',
+    component: () => import('../views/products/ProductEdit.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+router.beforeResolve((to, from, next) => {
+  if (to.name) {
+      NProgress.start()
+  }
+  next()
+});
+
+router.afterEach(() => {
+  NProgress.done()
+});
+
+export default router
